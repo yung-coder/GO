949b594986bcc55c256f377ec76d83fc6c69c8db
yung-coder <chandel.sanjeev5@gmail.com>
2023-04-04 22:53:13 +0530

    Array

---
+++ array/array.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var fruitlist [4]string
+
+	fruitlist[0] = "Apple";
+	fruitlist[1] = "Mickle";
+	fruitlist[2] = "Pickel";
+
+	fmt.Println(fruitlist);
+
+}
