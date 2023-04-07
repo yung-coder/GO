2bc5ee53335ab23355045e6cb4989fe37dab1efd
yung-coder <chandel.sanjeev5@gmail.com>
2023-04-07 23:30:30 +0530

    routing

--- API/main.go
+++ API/main.go
@@ -1,12 +1,20 @@
 package main
 
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
 // Model  for course  -- file
 
 type Course struct {
-	CourseId string `json:"coursed"`
-	CourseName string `json:"coursename"`
-	CoursePrice int `json:"price"`
-	Author *Author
+	CourseId    string `json:"coursed"`
+	CourseName  string `json:"coursename"`
+	CoursePrice int    `json:"price"`
+	Author      *Author
 }
 
 type Author struct {
@@ -16,14 +24,47 @@ type Author struct {
 
 // fake db
 
-var courses []Course;
+var courses []Course
 
-// middleware , helper - file 
+// middleware , helper - file
 
-func (c * Course) IsEmpty() bool {
+func (c *Course) IsEmpty() bool {
 	return c.CourseId == "" && c.CourseName == ""
 }
 
 func main() {
 
+}
+
+// controllers - file
+
+// serve home  route
+
+func serveHome(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("<h1>Buliding FIRST API USING GO</h1>"))
+}
+
+func getALLCourses(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get all Courses")
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(courses)
+}
+
+func getOneCourse(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("Get one Course")
+	w.Header().Set("Content-Type", "application/json")
+
+	// grab id from req
+	params := mux.Vars(r)
+
+	// find matching id and retur it
+
+	for _, course := range courses {
+		if course.CourseId == params["id"] {
+			json.NewEncoder(w).Encode(course)
+			return
+		}
+	}
+
+	json.NewEncoder(w).Encode("No Course Found")
 }
