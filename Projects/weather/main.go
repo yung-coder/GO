54af3208f96079a32057b4a0341aa55b0a8c5a2d
yung-coder <chandel.sanjeev5@gmail.com>
2023-04-11 23:21:37 +0530

    Projects

---
+++ Projects/weather/main.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"strings"
+)
+
+type weatherData struct {
+	Name string `json:"name"`
+	Main struct {
+		Kelvin float64 `json:"temp"`
+	} `json:"main"`
+}
+
+func query(city string) (weatherData, error) {
+	resp, err := http.Get("https://api.openweathermap.org/data/2.5/weather?&appid=9f66153982a275eb857428a4be592907" + "&q=" + city)
+	if err != nil {
+		return weatherData{}, err
+	}
+
+	// defer resp.Body.Close()
+
+	var d weatherData
+	if err := json.NewDecoder(resp.Body).Decode(&d); err != nil {
+		return weatherData{}, err
+	}
+
+	return d, nil
+}
+
+func main() {
+	http.HandleFunc("/weather/", func(w http.ResponseWriter, r *http.Request) {
+		city := strings.SplitN(r.URL.Path, "/", 3)[2]
+		data, err := query(city)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		w.Header().Set("Content-Type", "application/json; charset=utf-8")
+		json.NewEncoder(w).Encode(data)
+	})
+	http.ListenAndServe(":3000", nil)
+}
