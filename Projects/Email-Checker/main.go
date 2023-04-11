7bbadb84aebed6de58efd64d6d614cec23a5d5fa
yung-coder <chandel.sanjeev5@gmail.com>
2023-04-11 16:40:23 +0530

    Learning by building

---
+++ Projects/Email-Checker/main.go
@@ -0,0 +1,71 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"os"
+	"strings"
+)
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	for scanner.Scan() {
+		checkDomain(scanner.Text())
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal("Error could not read from input: %v \n", err)
+	}
+
+}
+
+func checkDomain(domain string) {
+
+	var hasMx, hasSPF, hasDMARC bool
+
+	var spfRecord, dmarcRecord string
+
+	mxRecords, err := net.LookupMX(domain)
+
+	if err != nil {
+		log.Printf("Eroor: %v\n", err)
+	}
+
+	if len(mxRecords) > 0 {
+		hasMx = true
+	}
+
+	txtRecords, err := net.LookupTXT(domain)
+
+	if err != nil {
+		log.Printf("Eroor: %v\n", err)
+	}
+
+	for _, record := range txtRecords {
+		if strings.HasPrefix(record, "v=spf1") {
+			hasSPF = true
+			spfRecord = record
+			break
+		}
+	}
+
+	dmarcRecords, err := net.LookupTXT("_dmarc." + domain)
+
+	if err != nil {
+		log.Printf("Eroor: %v\n", err)
+	}
+
+	for _, record := range dmarcRecords {
+		if strings.HasPrefix(record, "v=DMARC1") {
+			hasDMARC = true
+			dmarcRecord = record
+			break
+		}
+	}
+
+	fmt.Printf("%v , %v , %v , %v , %v , %v", domain, hasMx, hasSPF, spfRecord, hasDMARC, dmarcRecord)
+
+}
