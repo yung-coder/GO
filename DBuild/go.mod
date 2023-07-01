f269af528b700738c49581ee8cb341fc37beb1eb
yung-coder <chandel.sanjeev5@gmail.com>
2023-07-01 23:54:49 +0530

    db build

--- DBuild/go.mod
+++ DBuild/go.mod
@@ -2,4 +2,4 @@ module DBuild
 
 go 1.20
 
-require github.com/jcelliott/lumber v0.0.0-20160324203708-dd349441af25 // indirect
+require github.com/jcelliott/lumber v0.0.0-20160324203708-dd349441af25
--- DBuild/main.go
+++ DBuild/main.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
-	"github.com/jcelliott/lumber"
+	"io/ioutil"
 	"os"
+	"path/filepath"
 	"sync"
+
+	"github.com/jcelliott/lumber"
+	"golang.org/x/text/collate"
 )
 
 const version = "1.0.0"
@@ -29,34 +34,152 @@ type (
 )
 
 type Options struct {
-   Logger
+	Logger
+}
+
+func New(dir string, options *Options) (*Driver, error) {
+	dir = filepath.Clean(dir)
+
+	opts := Options{}
+
+	if options != nil {
+		opts = *options
+	}
+
+	if opts.Logger == nil {
+		opts.Logger = lumber.NewConsoleLogger((lumber.INFO))
+	}
+
+	driver := Driver{
+		dir:     dir,
+		mutexes: make(map[string]*sync.Mutex),
+		log:     opts.Logger,
+	}
+
+	if _, err := os.Stat(dir); err != nil {
+		opts.Logger.Debug("Using '%s' ", dir)
+		return &driver, nil
+	}
+
+	opts.Logger.Debug("Creating the database at '%s'....", dir)
+	return &driver, os.Mkdir(dir, 0755)
+
+}
+
+func stat(path string) (fi os.FileInfo, err error) {
+	if fi, err = os.Stat(path); os.IsNotExist(err) {
+		fi, err = os.Stat(path + ".json")
+	}
+	return
+}
+
+func (d *Driver) write(collection, resource string, v interface{}) error {
+	if collection == "" {
+		return fmt.Errorf("Missing collection no place to save record")
+	}
+
+	if resource == "" {
+		return fmt.Errorf("Missing resource - unable to save record")
+	}
+
+	mutex := d.getorCreateMitex(collection)
+	mutex.Lock()
+	defer mutex.Unlock()
+
+	dir := filepath.Join(d.dir, collection)
+	fnPath := filepath.Join(dir, resource+".json")
+	tmpPath := fnPath + ".tmp"
+
+	if err := os.MkdirAll(dir, 0755); err != nil {
+		return err
+	}
+
+	b, err := json.MarshalIndent(v, "", "\t")
+
+	if err != nil {
+		return err
+	}
+
+	b = append(b, byte('\n'))
+
+	if err := ioutil.WriteFile(tmpPath, b, 0644); err != nil {
+		return err
+	}
+
+  return os.Rename(tmpPath, fnPath); 
 }
 
 
-func New()() {
+
+func (d *Driver) Read(collection, resource string, v interface{}) error {
+
+	if collection == "" {
+		return fmt.Errorf("Missing collection no place to save record")
+	}
+
+	if resource == "" {
+		return fmt.Errorf("Missing resource - unable to save record")
+	}
+
+   record := filepath.Join(d.dir , collection , resource);
+
+  if _, err := stat(record); err != nil {
+     return err;
+  }
+
+  b , err := ioutil.ReadFile(record + ".json");
+
+  if err != nil {
+     return err;
+  }
+
+  return json.Unmarshal(b , &v);
+
+}
+
+func (d* Driver) ReadAll(collection string)([]string , error) {
+  if collection == "" {
+      return nil, fmt.Errorf("Missing Collection");
+  }
+
+  dir := filepath.Join(d.dir , collection);
+
+  if _ , err := stat(dir); err != nil {
+     return nil , err;
+  }
    
-}
+  files, _ := ioutil.ReadDir(dir);
 
+  var records []string;
 
-func (d* Driver) write() error {
+  for _ , file := range files {
+    b , err := ioutil.ReadFile(filepath.Join(dir, file.Name()));
 
-}
+    if err != nil {
+       return nil , err
+    }
 
-func (d* Driver) Read () error {
-
-}
-
-func (d* Driver) ReadAll()() {
-
-}
-
-func (d* Driver) Delet() error {
+    records = append(records, string(b));
+  }
+  return records , nil;
 
 }
 
+func (d *Driver) Delet() error {
 
-func (d* Driver) getorCreateMitex() *sync.Mutex {
+}
 
+func (d *Driver) getorCreateMitex(collection string) *sync.Mutex {
+
+	d.mutex.Lock()
+	defer d.mutex.Unlock()
+	m, ok := d.mutexes[collection]
+
+	if !ok {
+		m = &sync.Mutex{}
+		d.mutexes[collection] = m
+	}
+	return m
 }
 
 type Address struct {
