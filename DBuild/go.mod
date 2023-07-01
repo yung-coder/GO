3f3822e31fc53b9677563d5d5596937c7ae629a5
yung-coder <chandel.sanjeev5@gmail.com>
2023-07-01 20:53:10 +0530

    db build

--- DBuild/go.mod
+++ DBuild/go.mod
@@ -1,3 +1,5 @@
 module DBuild
 
 go 1.20
+
+require github.com/jcelliott/lumber v0.0.0-20160324203708-dd349441af25 // indirect
--- DBuild/main.go
+++ DBuild/main.go
@@ -1,7 +1,125 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/jcelliott/lumber"
+	"os"
+	"sync"
+)
 
-func main ()  {
-  fmt.Println("DBuild Travel");
+const version = "1.0.0"
+
+type (
+	Logger interface {
+		Fatal(string, ...interface{})
+		Error(string, ...interface{})
+		Warn(string, ...interface{})
+		Info(string, interface{})
+		Debug(string, ...interface{})
+		Trace(string, ...interface{})
+	}
+
+	Driver struct {
+		mutex   sync.Mutex
+		mutexes map[string]*sync.Mutex
+		dir     string
+		log     Logger
+	}
+)
+
+type Options struct {
+   Logger
+}
+
+
+func New()() {
+   
+}
+
+
+func (d* Driver) write() error {
+
+}
+
+func (d* Driver) Read () error {
+
+}
+
+func (d* Driver) ReadAll()() {
+
+}
+
+func (d* Driver) Delet() error {
+
+}
+
+
+func (d* Driver) getorCreateMitex() *sync.Mutex {
+
+}
+
+type Address struct {
+	City    string
+	State   string
+	Country string
+	Pincode json.Number
+}
+
+type User struct {
+	Name    string
+	Age     json.Number
+	Contact string
+	Company string
+	Address Address
+}
+
+func main() {
+	dir := "./"
+
+	db, err := New(dir, nil)
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+
+	employees := []User{
+		{"Jhon", "23", "696969699", "Infosys", Address{"palampur", "hp", "india", "14352"}},
+		{"Jogi", "26", "6964569699", "Infosysh", Address{"delhi", "it", "india", "14332"}},
+		{"rock", "28", "696964799", "Infosysbb", Address{"up", "gp", "india", "14344"}},
+	}
+
+	for _, value := range employees {
+		db.write("users", value.Name, User{
+			Name:    value.Name,
+			Age:     value.Age,
+			Contact: value.Contact,
+			Company: value.Company,
+			Address: value.Address,
+		})
+	}
+
+	records, err := db.ReadAll("users")
+	if err != nil {
+		fmt.Println("Error", err)
+	}
+	fmt.Println(records)
+
+	allusers := []User{}
+
+	for _, f := range records {
+		employeeFound := User{}
+		if err := json.Unmarshal([]byte(f), &employeeFound); err != nil {
+			fmt.Println("Errors", err)
+		}
+		allusers = append(allusers, employeeFound)
+	}
+
+	if err := db.Delet("user", "jhon"); err != nil {
+		fmt.Println("Error", err)
+	}
+
+	if err := db.DeletAll("user", ""); err != nil {
+		fmt.Println("Error", err)
+	}
+
 }
