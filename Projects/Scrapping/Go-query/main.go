3e668090de6942306025ef8a9c6cd07b2dd96f81
yung-coder <chandel.sanjeev5@gmail.com>
2023-07-23 01:56:13 +0530

    scrapping some demo data

--- Projects/Scrapping/Go-query/main.go
+++ Projects/Scrapping/Go-query/main.go
@@ -4,18 +4,33 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
-	blogTitles, err := GetLatestBlogTitles("https://www.friends2support.org/inner/news/listDonatedBloodUsers.aspx");
+	blogTitles, err := GetLatestBlogTitles("https://www.friends2support.org/inner/news/listDonatedBloodUsers.aspx")
 
 	if err != nil {
 		log.Println(err)
 	}
 
-	fmt.Println("Blog Titles")
+	file, err := os.Create("data.txt")
+
+	if err != nil {
+		log.Println(err)
+	}
+
+	defer file.Close()
+
+	_, err2 := file.WriteString(blogTitles)
+
+	if err2 != nil {
+		println(err)
+	}
+
+	fmt.Println("DATA Extracted")
 	fmt.Println(blogTitles)
 
 }
@@ -35,8 +50,15 @@ func GetLatestBlogTitles(url string) (string, error) {
 
 	titles := ""
 
-	doc.Find("table").Each(func(i int, s *goquery.Selection) {
-		titles += "-" + s.Text() + "\n"
+	doc.Find("#dgLastBloodDonated").Each(func(i int, s *goquery.Selection) {
+		// titles += "-" + s.Text() + "\n"
+		s.Find("tbody").Each(func(i int, s *goquery.Selection) {
+			s.Find("tr").Each(func(i int, s *goquery.Selection) {
+         fmt.Println(i);
+         check :=  fmt.Sprintf("#dgLastBloodDonated_lblPlace_%d" , i);
+				 titles += s.Find(check).Text();
+			})
+		})
 	})
 
 	return titles, nil
